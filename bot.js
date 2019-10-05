c415a96bcbe71937d33fcea88aa315c97debb992
omar159525212 <55595771+omar159525212@users.noreply.github.com>
2019-10-05 04:56:44 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,54 +1,41 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-const bugs = require("./Report.json")
-const fs = require("fs")
-var prefix = "!";
- 
- 
-client.on("message", TheReport => {
- 
-  const args = TheReport.content.slice(prefix.length).trim().split(/ +/g);
- 
-if(TheReport.content.startsWith(prefix + "report")) {
-  TheReport.delete();
-  if(TheReport.content === '${prefix}report') return TheReport.channel.send("**write your bug**").then(msgR =>msgR.delete(4000));
- bugs[TheReport.author.id] = {
-  message: args.slice(1).join(""),
-  by: TheReport.author.id
- }
-fs.writeFile("./Report.json", JSON.stringify(bugs, null , 4), err =>{
-  console.log(err);
-  })
-  }
- 
-}
+const prefix = "-"
  
  
  
-)
  
  
-client.on("message", msg => {
-  if(msg.author.id != "575229517828849674") return msg.channel.send("you cant do this command").then(newmessage =>newmessage.delete(4000));
-  if (msg.content.startsWith(prefix + "bugs")){
-    msg.delete();
-    let data = undefined;
-  for(i in bugs){
-      if (data === undefined) {
-        data = "";
-      }
-      let data1 = bugs[i].message
-      let data2 = bugs[i].by
-      const stuff = `${data1} **By** <@${data2}>`;
-      data += (stuff) + "n";
-    }
-    if (data !== undefined) {
-      const richEmbed = new Discord.RichEmbed();
-      richEmbed.addField("Messages", data)
-      msg.channel.send(richEmbed)
-    }
-  }
-})
+ 
+ 
+console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+console.log('         [Wait please .. ]       ')
+console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+client.on('ready', () => {
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
+    console.log('')
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log(`Logged in as [ ${client.user.tag}! ]`);
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log('[           BOT IS ONLINE         ]')
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log('[        info         ]')
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+ 
+client.user.setGame("الستريم هنا", "https://www.twitch.tv/alpha");
+});
 
 
 
