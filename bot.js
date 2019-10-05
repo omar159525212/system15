08f2d1d85ab183f7a39c1d700deef3fbac208fe3
omar159525212 <55595771+omar159525212@users.noreply.github.com>
2019-10-05 03:50:49 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -181,88 +181,29 @@ if (message.author.bot) return;
 });
 
 
-
-
 client.on('message', message => {
-if (message.author.bot) return;
-  let args = message.content.split(" ");
- 
-  let command = args[0];
- 
-  let user = message.mentions.users.first();
- 
-  let bantime = args[2];
-   
-  let reasonban = message.content.split(" ").slice(3).join(" ");
-   
-  let timefilter;
- 
-  if (command == prefix + "ban") {
-     
-    if(!message.channel.guild){
-    message.channel.send("**لا يمكن استعمال هذا الأمر في الخاص**");
-}
-    if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) {
-        message.channel.send("**يجب ان يكون لديك خاصية `BAN_MEMBERS`**");
-    }
-    if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) {
-        message.channel.send("**البوت لا يمتلك خاصية `BAN_MEMBERS`**");
-    }
-   
-    if (!user){
-        message.channel.send("**يجب عليك إختيار الشخص المراد حظره**");
-    }else if (!bantime){
-        message.channel.send(`**
-        يجب عليك اختيار وقت الحظر
-        15m حظر ربع ساعه
-        30m حظر نصف ساعه
-        1h حظر ساعه
-        3h حظر ثلاث ساعات
-        1d حظر يوم كامل
-        3d حظر ثلاث ايام
-        1w حظر اسبوع
-        1mon حظر شهر كامل
-        **`);
-    }else if (!reasonban){
-        message.channel.send("**يجب عليك إدراج سبب الحظر**");
-    }
-    if (message.guild.member(user).hasPermission("BAN_MEMBERS")){
-    message.channel.send("**لا يمكن طرد هذا الشخص , فهو من الإدارة**");
-    } else {
-// By Alpha Codes - AboKhalil 26/7/2019
-    if (bantime = "15m") {
-    timefilter = 150000;
-    } else if (bantime = "30m") {
-        timefilter = 300000;
-    } else if (bantime = "1h") {
-        timefilter = 600000;
-    } else if (bantime = "3h") {
-        timefilter = 1800000;
-    } else if (bantime = "1d") {
-        timefilter = 14400000;
-    } else if (bantime = "3d") {
-        timefilter = 43200000;
-    } else if (bantime = "1w") {
-        timefilter = 100800000;
-    } else if (bantime = "1mon"){
-        timefilter = 432000000;
-    }
-    message.guild.member(user).ban()
-    message.channel.send("**The Member Was Banned **" + user.tag + " **By** : " + message.author.tag);
-    message.channel.send("**Reason : __" + reasonban + "__**");
-   
-    user.send("**You are Banned By** : " + message.author.tag);
-    user.send("**Reason : __" + reasonban + "__**");
-setTimeout(() => {
- 
-  message.guild.unban(bannedman);
- 
-        }, timefilter);
-    }
- }
- // By Alpha Codes - AboKhalil 26/7/2019
-});
+  if (message.author.bot) return;
 
+  let args = message.content.split(" ");
+  
+  let command = args[0];
+  
+  let messagecount = args[1];
+  
+    if(command == prefix + "clear") {
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) {
+		message.channel.send("يجب ان تمتلك خاصية `MANAGE_MESSAGES` ")
+	} else if (!messagecount) {
+		message.channel.send("**قم بإدراج عدد الرسائل المراد حذفها**")
+	}else {
+		message.channel.bulkDelete(messagecount);
+        message.channel.send("**رسالة `" + messagecount + "` لقد تم حذف**").then(mes => 
+		mes.delete(3000)
+		);    
+	 }
+   }
+  // By Alpha Codes - AboKhalil 26/7/2019
+});
 
 
 
