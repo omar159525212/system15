709bad66de36ed1b1b2f687c7569f3284fa5d801
omar159525212 <55595771+omar159525212@users.noreply.github.com>
2019-10-06 09:11:50 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,29 +17,71 @@ const math = require('math-expression-evaluator');   //WESO#0001
 const figlet = require('figlet');   //WESO#0001
 const fs = require('fs');  //WESO#0001
 const ms = require('ms');  //WESO#0001
-const prefix = '*'
+const prefix = '-'
 
-client.on('message', message => {
-    if (message.content.startsWith(prefix + 'sug')) {
-        if (message.author.bot) return
-        if (!message.guild) return message.reply('**:x: This Commands Just In Server**').then(v => {v.react('❌')})
-        var args =  message.content.split(' ').slice(1).join(' ')
-        if (!args) return message.reply(' (sug)اكتب اقتراحك بعد ').then(c => {c.delete(5000)})
-        let Room = message.guild.channels.find(`name`, "suggestions")
-        if (!Room) return message.channel.send(" (suggestions)لا يوجد شات.").then(d => d.react('❌'))
-        let embed = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .setAuthor(`Vote on ${message.author.username}'s suggestion`, message.author.avatarURL)
-       .addField('**Suggestion**',`${args}`)
-       .setThumbnail(message.author.avatarURL)
-       .setFooter(`ID: ${message.author.id}`)
-       Room.sendEmbed(embed).then(c => {
-           c.react('✅').then(() =>
-               c.react('❌'))
-           
-       }).catch(e => console.error(e)
-       )
-   }
+ var filter = m => m.author.id === message.author.id;
+  if(message.content.startsWith(prefix + "giveaway")) {
+ 
+    if(!message.guild.member(message.author).hasPermission('MANAGE_GUILD')) return message.channel.send(':heavy_multiplication_x:| **يجب أن يكون لديك خاصية التعديل على السيرفر**');
+    message.channel.send(`:eight_pointed_black_star:| **اكتب اسم روم الجيفاواي**`).then(msg => {
+      message.channel.awaitMessages(filter, {
+        max: 1,
+        time: 20000,
+        errors: ['time']
+      }).then(collected => {
+        let room = message.guild.channels.find('giveaway' , collected.first().content);
+        if(!room) return message.channel.send(':heavy_multiplication_x:| **i Found It :(**');
+        room = collected.first().content;
+        collected.first().delete();
+        msg.edit(':eight_pointed_black_star:| **كم تريد وقت الجيفواي**').then(msg => {
+          message.channel.awaitMessages(filter, {
+            max: 1,
+            time: 20000,
+            errors: ['time']
+          }).then(collected => {
+            if(isNaN(collected.first().content)) return message.channel.send(':heavy_multiplication_x:| **انا لم افهم هاذا الوقت**');
+            duration = collected.first().content * 60000;
+            collected.first().delete();
+            msg.edit(':eight_pointed_black_star:| **ما هي الجائزة؟**').then(msg => {
+              message.channel.awaitMessages(filter, {
+                max: 1,
+                time: 20000,
+                errors: ['time']
+              }).then(collected => {
+                title = collected.first().content;
+                collected.first().delete();
+                msg.delete();
+                message.delete();
+                try {
+                  let giveEmbed = new Discord.RichEmbed()
+                  .setDescription(`**${title}** \nReact With 🎉 To Enter! \nTime remaining : ${duration / 60000} **Minutes**\n **Created at :** ${hours}:${minutes}:${seconds} ${suffix}`)
+                  .setFooter(message.author.username, message.author.avatarURL);
+                  message.guild.channels.find("name" , room).send(' :heavy_check_mark: **تم صنع الجيفواي** :heavy_check_mark:' , {embed: giveEmbed}).then(m => {
+                     let re = m.react('🎉');
+                     setTimeout(() => {
+                       let users = m.reactions.get("🎉").users;
+                       let list = users.array().filter(u => u.id !== m.author.id !== client.user.id);
+                       let gFilter = list[Math.floor(Math.random() * list.length) + 0]
+                       let endEmbed = new Discord.RichEmbed()
+                       .setAuthor(message.author.username, message.author.avatarURL)
+                       .setTitle(title)
+                       .addField('Giveaway Ended !🎉',`Winners : ${gFilter} \nEnded at :`)
+                       .setTimestamp()
+                     m.edit('** 🎉 GIVEAWAY ENDED 🎉**' , {embed: endEmbed});
+                    message.guild.channels.find("name" , room).send(`**Congratulations ${gFilter}! انت ربحت الجيفواي \`${title}\`**` , {embed: {}})
+                     },duration);
+                   });
+                } catch(e) {
+                message.channel.send(`:heavy_multiplication_x:| **ليست لدي الصلاحيات الكافية**`);
+                 console.log(e);
+               }
+             });
+           });
+         });
+       });
+     });
+   });
+ }
 });
 
 
